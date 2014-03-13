0ac926ff435b8386b455ab43379e10d7c8803416
Erik Michaels-Ober <sferik@gmail.com>
2014-03-13 03:49:22 +0100

    JSON keys are parsed into strings, not symbols

--- spec/helper.rb
+++ spec/helper.rb
@@ -5,7 +5,7 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[SimpleCov::Formatter:
 
 SimpleCov.start do
   add_filter '/spec/'
-  minimum_coverage(99.3)
+  minimum_coverage(99.27)
 end
 
 require 'twitter'
